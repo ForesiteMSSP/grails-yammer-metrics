077afe7782b732637d65c62d849bf74c19d6108a
Pierre MOYSAN <pierre.moysan49@gmail.com>
2015-12-11 09:29:03 +0100

    Upgrade to : Grails 2.4.4 ; Metrics 3.1.2 ; release plugin 3.1.2 ; rest-client-builder plugin 2.1.1. Set project version to 3.1.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,7 +6,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-metrics.core.version = "3.0.1"
+metrics.core.version = "3.1.2"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -34,7 +34,7 @@ grails.project.dependency.resolution = {
     }
 	
 	plugins {
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
             export = false
         }
 
@@ -47,9 +47,7 @@ grails.project.dependency.resolution = {
 	
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-		runtime "com.codahale.metrics:metrics-core:${metrics.core.version}"
-		runtime "com.codahale.metrics:metrics-servlets:${metrics.core.version}"
-
+		runtime "io.dropwizard.metrics:metrics-core:${metrics.core.version}"
+		runtime "io.dropwizard.metrics:metrics-servlets:${metrics.core.version}"
     }
 }
