33a847900a0113caa8d3701e80285a7e0b47e36a
Randal Pitt <randal.pitt@foresite.com>
2019-04-29 16:43:12 +0100

    Update grails version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -33,16 +33,14 @@ grails.project.dependency.resolution = {
 
     }
 	
-	plugins {
-		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
+    plugins {
+	build ':release:3.1.2', ':rest-client-builder:2.1.1', {
             export = false
         }
 
         build ":codenarc:0.19", {
             export = false
         }
-
-
     }
 	
     dependencies {
